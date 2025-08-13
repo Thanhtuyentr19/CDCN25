53d885dd6e0029d78e5a503cb429af1881d1c173
thein.tyne <tuyenthanh2882@gmail.com>
2025-08-13 09:22:14 +0700

    Part2

---
+++ djangotutorial/polls/models.py
@@ -0,0 +1,31 @@
+# from django.db import models
+
+# class Question(models.Model):
+#     question_text = models.CharField("Câu hỏi", max_length=200)
+#     pub_date = models.DateTimeField("Ngày phát hành")
+
+#     def __str__(self):
+#         return self.question_text
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices")
+#     choice_text = models.CharField("Phương án", max_length=200)
+#     votes = models.IntegerField(default=0)
+
+#     def __str__(self):
+#         return self.choice_text
+from django.db import models
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+     return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
